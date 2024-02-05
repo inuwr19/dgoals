5e38d7d8495044b1154fbdf01a04bed8ecd2dc71
abdurrahmangrahadim <hadi@busanid.dev>
2024-02-05 23:57:16 +0700

    setup first

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::controller(App\Http\Controllers\frontendController::class)->group(functio
     //     Route::post('logout', 'logout')->name('logout');
     // });
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
